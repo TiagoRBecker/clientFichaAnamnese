a2725146be190fbdbfc28d8417a6b830625d59af
Tiago Becker <beckertiago09@gmail.com>
2025-09-24 10:03:22 -0300

    confir

--- src/components/Buttons/createOrderPayment.tsx
+++ src/components/Buttons/createOrderPayment.tsx
@@ -1,27 +1,49 @@
-"use client"
+"use client";
 import { usePayments } from "@/utils/Queries/usePayments";
 import { usePathname, useRouter } from "next/navigation";
+import Swal from "sweetalert2";
 
 const ButtonPayment = () => {
-    const router = useRouter()
+  const router = useRouter();
   const { createOrderPayment } = usePayments();
   const pathname = usePathname();
 
   const handleCreatePayment = async () => {
-  
-    createOrderPayment.mutate(undefined,{
-      async onSuccess(data, variables, context) {
-       
-          const newPath = data?.replace('/cart', pathname);
-          return router.replace(newPath)
-      },
-      async onError(error, variables, context) {
-        return alert("Estamos com problemas internos .Tente Novamente mais tarde.")
-      },
+    const confirm = await Swal.fire({
+      title: "Confirmar Pagamento",
+      text: "Ao confirmar, você será redirecionado para um ambiente protegido e certificado, garantindo total segurança na finalização do seu pagamento.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, continuar",
+      cancelButtonText: "Cancelar",
     });
+    if (confirm.isConfirmed) {
+      createOrderPayment.mutate(undefined, {
+        async onSuccess(data, variables, context) {
+          const newPath = data?.replace("/cart", pathname);
+          return router.replace(newPath);
+        },
+        async onError(error, variables, context) {
+          await Swal.fire(
+            "Internal Server Error",
+            "Estamos com problemas internos. Tente novamente mais tarde!",
+            "error"
+          );
+          return 
+        },
+      });
+    }
+    /*
+   
+    */
   };
   return (
-    <button  onClick={handleCreatePayment} className="w-full  mt-6 py-4 bg-[#336DFF] text-white rounded-full text-xlflex items-center justify-center">
+    <button
+      onClick={handleCreatePayment}
+      className="w-full  mt-6 py-4 bg-[#336DFF] text-white rounded-full text-xlflex items-center justify-center"
+    >
       Finalizar Compra
     </button>
   );
