445d3aa4cc76620bc48da81fd8ebfdec23cf1b9a
Tiago Becker <beckertiago09@gmail.com>
2025-10-02 14:13:22 -0300

    ajustes cart

--- src/components/Perfil/index.tsx
+++ src/components/Perfil/index.tsx
@@ -17,82 +17,80 @@ import Cart from "../checkout";
 
 const Perfil = () => {
   const { data: session, status } = useSession();
-
-  const [showCart, setShowCart] = useState(false);
-  useEffect(() => {}, [status]);
   const { listCart } = useCartHook();
+  const [showCart, setShowCart] = useState(false);
+  useEffect(() => {
+    if (status === "authenticated") {
+      listCart.refetch();
+    }
+  }, [status ,listCart.data]);
 
-  if(listCart.isLoading) return null
+  if (listCart.isLoading) return null;
   return (
     <div className="w-full  flex items-center justify-end  ">
-    
-    
-    
-          <div className="flex items-center gap-3">
-            <div
-              className="relative cursor-pointer flex items-center justify-end   "
-              onClick={() => setShowCart(true)}
-            >
-              <ShoppingBag size={30} color="#336DFF" strokeWidth={1} />
-              <p className="w-4 h-4 bg-[#31AF97] rounded-full text-white flex items-center justify-center top-0 right-0 absolute text-[8px]">
-                {listCart?.data?.cartItems?.length || 0}
+      <div className="flex items-center gap-3">
+        <div
+          className="relative cursor-pointer flex items-center justify-end   "
+          onClick={() => setShowCart(true)}
+        >
+          <ShoppingBag size={30} color="#336DFF" strokeWidth={1} />
+          <p className="w-4 h-4 bg-[#31AF97] rounded-full text-white flex items-center justify-center top-0 right-0 absolute text-[8px]">
+            {listCart?.data?.cartItems?.length || 0}
+          </p>
+        </div>
+        <Menu>
+          <MenuButton className="w-full h-full  ">
+            <div className="flex items-center gap-3 w-full  justify-end">
+              <p className="hidden md:capitalize font-medium md:flex gap-1 items-center truncate ">
+                {session?.user?.name}
+                <ChevronDown size={18} />
+              </p>
+              <p className="capitalize font-medium flex gap-1 items-center md:hidden ">
+                <UserRound size={30} color="#336DFF" strokeWidth={1} />
               </p>
             </div>
-            <Menu>
-              <MenuButton className="w-full h-full  ">
-                <div className="flex items-center gap-3 w-full  justify-end">
-                  <p className="hidden md:capitalize font-medium md:flex gap-1 items-center truncate ">
-                    {session?.user?.name}
-                    <ChevronDown size={18} />
-                  </p>
-                  <p className="capitalize font-medium flex gap-1 items-center md:hidden ">
-                    <UserRound size={30} color="#336DFF" strokeWidth={1} />
-                  </p>
-                </div>
-              </MenuButton>
+          </MenuButton>
 
-              <MenuList
-                bg="white"
-                p={1}
-                shadow="md"
-                display={"flex"}
-                flexDirection={"column"}
-                gap={2}
-              >
-                <MenuItem
-                  as={Link}
-                  href="/library"
-                  className="flex items-center gap-2"
-                >
-                  <BookOpenText strokeWidth="1" />
-                  Documentos
-                </MenuItem>
+          <MenuList
+            bg="white"
+            p={1}
+            shadow="md"
+            display={"flex"}
+            flexDirection={"column"}
+            gap={2}
+          >
+            <MenuItem
+              as={Link}
+              href="/library"
+              className="flex items-center gap-2"
+            >
+              <BookOpenText strokeWidth="1" />
+              Documentos
+            </MenuItem>
 
-                <MenuItem
-                  as={Link}
-                  href="/orders"
-                  className="flex items-center gap-2"
-                >
-                  <BookCopy strokeWidth="1" />
-                  Pedidos
-                </MenuItem>
+            <MenuItem
+              as={Link}
+              href="/orders"
+              className="flex items-center gap-2"
+            >
+              <BookCopy strokeWidth="1" />
+              Pedidos
+            </MenuItem>
 
-                <MenuItem
-                  onClick={() => signOut({ callbackUrl: "/" })}
-                  className="flex items-center gap-2"
-                >
-                  <LogOut strokeWidth="1" />
-                  Sair
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          </div>
+            <MenuItem
+              onClick={() => signOut({ callbackUrl: "/" })}
+              className="flex items-center gap-2"
+            >
+              <LogOut strokeWidth="1" />
+              Sair
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
 
-          {listCart?.data?.cartItems?.length > 0 && showCart && (
-            <Cart handleCloseMenu={() => setShowCart(false)} />
-          )}
-      
-  
+      {listCart?.data?.cartItems?.length > 0 && showCart && (
+        <Cart handleCloseMenu={() => setShowCart(false)} />
+      )}
     </div>
   );
 };
