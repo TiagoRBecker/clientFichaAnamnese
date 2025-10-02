8762a319c04a565e7ccd656ea2f041258e7d890d
Tiago Becker <beckertiago09@gmail.com>
2025-10-02 17:32:29 -0300

    nt

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import CardList from "@/components/cardlist";
 import Loading from "@/components/Loading";
+import NotFound from "@/components/NotFound/indext";
 import { useAllDocs } from "@/utils/Queries/useDocuments";
 
 import { useSearchParams } from "next/navigation";
@@ -10,19 +11,20 @@ const AllDocs = () => {
   const params = useSearchParams();
   const name = params.get("search");
   useEffect(() => {}, [name]);
-  const {data, isLoading} = useAllDocs(name as string);
- if(isLoading) return <Loading/>
+  const { data, isLoading } = useAllDocs(name as string);
+  if (isLoading) return <Loading />;
+  if (!data.length) return <NotFound />;
   return (
     <Suspense fallback={""}>
-    <div className="w-full h-screen ">
-      <div className="container mx-auto">
-        <h1 className="py-10">
-          Busca realizada para{" "}
-          <span className="text-2xl text-gray-500 uppercase">{name}</span>
-        </h1>
-         <CardList data={data} name="Categorias"  />
+      <div className="w-full min-h-screen">
+        <div className="container mx-auto">
+          <h1 className="py-10">
+            Busca realizada para{" "}
+            <span className="text-2xl text-gray-500 uppercase">{name}</span>
+          </h1>
+          <CardList data={data} name="Categorias" />
+        </div>
       </div>
-    </div>
     </Suspense>
   );
 };
